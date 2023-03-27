7c6c54be67af54c226dfa0711df4d8faba51ff30
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-03-27 01:48:13 -0300

    Testando alterações no servidor Expo

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#444',
+    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
