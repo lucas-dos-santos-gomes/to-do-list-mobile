9020a241b7af88076a7d11807002596a1c2f423a
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-03-27 02:06:24 -0300

    Importando tudo e finalizando App de teste

--- App.js
+++ App.js
@@ -1,22 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import Header from './src/components/Header';
+import Empty from './src/components/Empty';
+import Task from './src/components/Task';
+import Input from './src/components/Input';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const [data, setData] = useState([]);
+
+   const submitHandle = (value) => {
+      setData((prevTask) => {
+         return [
+            {
+               value: value,
+               key: Math.random().toString(),
+            },
+            ...prevTask
+         ]
+      });
+   };
+
+   const deleteItem = (key) => {
+      setData(prevTask => {
+         return prevTask.filter((task) => task.key != key);
+      });
+   };
+
+   return (
+      <View style={styles.container}>
+         <FlatList data={data} keyExtractor={(item) => item.key}
+            ListHeaderComponent={() => <Header />}
+            ListEmptyComponent={() => <Empty />}
+            renderItem={({ item }) => <Task item={item} deleteItem={deleteItem} />}
+         />
+         <View>
+            <Input submitHandle={submitHandle} />
+         </View>
+         <StatusBar style="light" />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#444',
-    color: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#121212',
+      alignItems: 'center',
+      flexDirection: 'column',
+      paddingHorizontal: 10,
+      paddingVertical: 70,
+   },
 });
